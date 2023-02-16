144098be43ad3cbc5198ae824078af4b609ebeb0
Ivan <Tomsk16121@yandex.ru>
2023-02-16 14:50:06 +0700

    init

---
+++ Calculator-MAD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator-MAD
+//
+//  Created by Ivan Semenov on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
